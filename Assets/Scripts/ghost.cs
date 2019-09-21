2f9d8413f69b880777a7464cc9adb67811023af4
Cameron Johnstone <johnstone.a.cameron@gmail.com>
2019-09-21 11:27:42 -0500

    Working: Player + Cloning

---
+++ Assets/Scripts/ghost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost : MonoBehaviour
+{
+    private float timeRemaining = 10f;
+
+    void Start() {
+
+    }
+    void FixedUpdate() {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining < 0) {
+            Destroy(gameObject);
+            Debug.Log("Object should be destroyed");
+        }
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    //Public Game Variables
     public float walkSpeed = 10f;
-    Rigidbody2D pRigid;
+    public GameObject ghostPrefab;
 
-    void Start()
-    {
+    //Private Game Variables
+    private Rigidbody2D pRigid;
+    private bool ghostControl = false;
+    private bool slowdown = false;
+
+    void Start() {
+        //obtain rigidbody object from attached player object
         pRigid = gameObject.GetComponent<Rigidbody2D>();
     }
-    void FixedUpdate()
-    {
-        //debug.log(time.deltaTime());    
-        pRigid.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * walkSpeed, 0.8f),
-            Mathf.Lerp(0, Input.GetAxis("Vertical") * walkSpeed, 0.8f));
+    void FixedUpdate() {
+
+        //if we're not controlling a ghost, get keyboard input and translate the player
+        if(!ghostControl) {
+            pRigid.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * walkSpeed, 0.8f),
+                Mathf.Lerp(0, Input.GetAxis("Vertical") * walkSpeed, 0.8f));
+        }
+        //after player control swaps to ghost, slow player to a nice stop rather than just have it freeze
+        if(slowdown && pRigid.velocity != new Vector2(0,0))
+        {
+            pRigid.velocity = pRigid.velocity * .9f;
+        }
+        if(Input.GetKeyDown("space")) {
+            Debug.Log("Space Pressed");
+            //Stop player from moving
+            slowdown = true;
+            //activate ghost control
+            ghostControl = true;
+            //create ghost gameObject
+            Instantiate(ghostPrefab, this.transform.position, Quaternion.identity);
+        }
     }
 }
