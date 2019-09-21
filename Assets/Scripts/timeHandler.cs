e6f93b33392d15e083875e127dcb4812cf9e9475
Cameron Johnstone <johnstone.a.cameron@gmail.com>
2019-09-21 12:00:16 -0500

    -Project idea change

---
+++ Assets/Scripts/timeHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeHandler : MonoBehaviour
+{
+    public float timeRemaining = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining < 0)
+        {
+            //player.ghostControl = false;
+            //Destroy(gameObject);
+            Debug.Log("Object should be destroyed");
+        }
+    }
+}
