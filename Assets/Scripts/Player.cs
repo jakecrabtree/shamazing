c44ef9629c5397152525f0e790fe7df95bfcf035
PogEmblem <55610318+PogEmblem@users.noreply.github.com>
2019-09-21 20:27:22 -0500

    player animation
    pog

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 {
     //Public Game Variables
     public float walkSpeed = 5f;
+    public Animator animator;
+
+    Vector2 movement;
 
     //Private Game Variables
     private Rigidbody2D pRigid;
@@ -15,10 +18,26 @@ public class Player : MonoBehaviour
         //obtain rigidbody object from attached player object
         pRigid = gameObject.GetComponent<Rigidbody2D>();
     }
+
+     void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
     void FixedUpdate() {
 
         //if we're not controlling a ghost, get keyboard input and translate the player
         pRigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, Input.GetAxisRaw("Vertical") * walkSpeed);
         //after player control swaps to ghost, slow player to a nice stop rather than just have it freeze
+
+        //pRigid.MovePosition(pRigid.position +movement *walkSpeed *Time.fixedDeltaTime);
+        
+       
     }
 }
