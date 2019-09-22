4ce14b1770934571713a170a31cbdf0fe9fd0bb0
filipjnybob <ia81718@gmail.com>
2019-09-22 11:00:33 -0500

    Made background music continue between levels

---
+++ Assets/BackgroundAudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudioManager : MonoBehaviour
+{
+    public static BackgroundAudioManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
